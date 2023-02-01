0fa639f8e387cbba24aa58723a53b338ecf20c96
CiprianPocol <CiprianPocol@users.noreply.github.com>
2023-02-01 11:32:30 +0200

    code style

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 #include "Controller.h"
 #include "Map.h"
 
-char screen[screenH][screenW + 1] = {{0}}; //we'll paint everything in this matrix, then flush it onto the real screen
-char Texture[sqRes*sqRes];
-int xC = int(2.5f * sqRes), yC = int(2.5f * sqRes), angleC = 10, elevation_perc = 0; //viewer Current position, orientation and elevation_perc
+char screen[screenH][screenW + 1] = {{0}}; // we'll paint everything in this matrix, then flush it onto the real screen
+char Texture[sqRes * sqRes];
+int xC = int(2.5f * sqRes), yC = int(2.5f * sqRes), angleC = 10, elevation_perc = 0; // viewer Current position, orientation and elevation_perc
 
-void CastX(int xC, int yC, int angle, int& xHit, int& yHit) { //   hit vertical walls ||
-    //prepare as for 1st or 4th quadrant
+void CastX(int xC, int yC, int angle, int& xHit, int& yHit) { // hit vertical walls ||
+    // prepare as for 1st or 4th quadrant
     xHit = (xC / sqRes) * sqRes + sqRes;
     int dx = sqRes,   adjXMap = 0;
     float dy = sqRes * tanf(angle * 3.1416f / aroundh);
-    if ((aroundq < angle) && (angle < around3q)) { //2nd or 3rd quadrant
+    if ((aroundq < angle) && (angle < around3q)) { // 2nd or 3rd quadrant
         xHit -= sqRes;
         adjXMap = -1;
         dx = -dx;
@@ -28,12 +28,12 @@ void CastX(int xC, int yC, int angle, int& xHit, int& yHit) { //   hit vertical 
     yHit = (int)fyHit;
 }
 
-void CastY(int xC, int yC, int angle, int& xHit, int& yHit) { //   hit horizontal walls ==
-    //prepare as for 1st or 2nd quadrant
+void CastY(int xC, int yC, int angle, int& xHit, int& yHit) { // hit horizontal walls ==
+    // prepare as for 1st or 2nd quadrant
     yHit = (yC / sqRes) * sqRes + sqRes;
     int dy = sqRes,   adjYMap = 0;
     float dx = sqRes / tanf(angle * 3.1416f / aroundh);
-    if (angle > aroundh) { //3rd or 4th quadrants
+    if (angle > aroundh) { // 3rd or 4th quadrants
         yHit -= sqRes;
         adjYMap = -1;
         dy = -dy;
@@ -49,45 +49,45 @@ void CastY(int xC, int yC, int angle, int& xHit, int& yHit) { //   hit horizonta
 void Render() {
     memset(screen, ' ', sizeof(screen));
 
-    const int viewerToScreen_sq = sq(screenWh) * 3; //FOV = 60 degs => viewerToScreen = screenWh * sqrt(3)
+    const int viewerToScreen_sq = sq(screenWh) * 3; // FOV = 60 degs => viewerToScreen = screenWh * sqrt(3)
     for (int col = 0; col < screenW; col++) {
         int xHit, yHit, xY, yY;
         int ang = (screenWh - col + angleC + around) % around;
-        ang += (ang % aroundq == 0) ? 1 : 0; //avoid infinite slope
+        ang += (ang % aroundq == 0) ? 1 : 0; // avoid infinite slope
         CastX(xC, yC, ang, xHit, yHit);
         CastY(xC, yC, ang, xY, yY);
-        if (abs(xC - xY) < abs(xC - xHit)) //choose the nearest hit point
+        if (abs(xC - xY) < abs(xC - xHit)) // choose the nearest hit point
             xHit = xY, yHit = yY;
 
-        int h = int(sqRes * sqrt((viewerToScreen_sq + sq(screenWh - col)) / (float)(sq(xC - xHit) + sq(yC - yHit) + 1))); //+1 avoids division by zero
-        int Dh_fp = (sqRes << 10) / h; //1 row in screen space represents this many rows in texture space; use fixed point
+        int h = int(sqRes * sqrt((viewerToScreen_sq + sq(screenWh - col)) / float(sq(xC - xHit) + sq(yC - yHit) + 1))); // +1 avoids division by zero
+        int Dh_fp = (sqRes << 10) / h; // 1 row in screen space represents this many rows in texture space; use fixed point
         int textureRow_fp = 0;
         int minRow = ((100 - elevation_perc) * (2 * screenHh - h) / 2 + elevation_perc * screenHh) / 100;
         int maxRow = min(minRow + h, screenH);
-        if (minRow < 0) { //clip
+        if (minRow < 0) { // clip
             textureRow_fp = -(minRow * Dh_fp);
             minRow = 0;
         }
 
         for (int row = minRow; row < maxRow; row++, textureRow_fp += Dh_fp)
-            screen[row][col] = *(Texture + (textureRow_fp >> 10) * sqRes + (xHit + yHit) % sqRes); //textureColumn = (xHit + yHit) % sqRes
+            screen[row][col] = *(Texture + (textureRow_fp >> 10) * sqRes + (xHit + yHit) % sqRes); // textureColumn = (xHit + yHit) % sqRes
     }
 
     for (int row = 0; row < screenH; row++)
         screen[row][screenW] = '\n';
 
-    //flush the screen matrix onto the real screen
-    system("cls"); //clear the (real) screen
-    screen[screenH - 1][screenW - 1] = 0; //avoid scrolling one row up when the screen is full
+    // flush the screen matrix onto the real screen
+    system("cls"); // clear the (real) screen
+    screen[screenH - 1][screenW - 1] = 0; // avoid scrolling one row up when the screen is full
     printf("%s", (char*)screen);
-    Sleep(6); //tune this one to get less flickering on a specific PC
+    Sleep(6); // tune this one to get less flickering on a specific PC
 }
 
 int main() {
-    //generate texture
+    // generate texture
     for (int i = 0; i < sqRes; i++)
         for (int j = 0; j < sqRes; j++)
-            *(Texture + i*sqRes + j) = ((0.1*sqRes < i) && (i < 0.7*sqRes) && (0.2*sqRes < j) && (j < 0.8*sqRes)) ? 'O' : '*';
+            *(Texture + i * sqRes + j) = ((0.1 * sqRes < i) && (i < 0.7 * sqRes) && (0.2 * sqRes < j) && (j < 0.8 * sqRes)) ? 'O' : '*';
 
     while (1)
         if (loopController(xC, yC, angleC, around))
